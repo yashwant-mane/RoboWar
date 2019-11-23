360fc52c0782bb76e6af4e5e956d0f5ad6c44a1f
Yashwant-mane <maneyashwant@65gmail.com>
2019-11-23 11:40:02 +0530

    Updates

---
+++ precache-manifest.8bbd752139990d32abb748b2935a83de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd8d7ae89a1f1e46cc17efdf27e77f4",
+    "url": "/RoboWar/index.html"
+  },
+  {
+    "revision": "662da9db28202dd4f005",
+    "url": "/RoboWar/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f54540c8bd3a18e1c072",
+    "url": "/RoboWar/static/css/main.a51707d4.chunk.css"
+  },
+  {
+    "revision": "662da9db28202dd4f005",
+    "url": "/RoboWar/static/js/2.1fcf6901.chunk.js"
+  },
+  {
+    "revision": "f54540c8bd3a18e1c072",
+    "url": "/RoboWar/static/js/main.6b4a6504.chunk.js"
+  },
+  {
+    "revision": "d3f08c933b4be38b1ab3",
+    "url": "/RoboWar/static/js/runtime-main.3e318536.js"
+  }
+]);
